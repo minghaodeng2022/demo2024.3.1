b5ebe793e495de1fc043a1deb4a62dba90dbeb12
dengminghao <minghaodeng@huamod.com>
2024-03-15 11:23:21 +0800

    first commit

---
+++ src/pytest_result_sender/plugin.py
@@ -0,0 +1,12 @@
+#!python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/2/29 10:17
+# @Author  : 邓明浩
+# @File    : plugin.py
+# @Software: PyCharm
+
+from datetime import datetime
+
+def pytest_configure():
+    pass
+    # 配置加载完毕之后执行，测试用例执行前执行#
